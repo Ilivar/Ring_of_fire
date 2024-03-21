e6f81e21c4fff9b0fe0bac9ebc824db5ea90f14c
Giuliano Gioia <giulianogioia86@gmail.com>
2024-03-21 18:14:10 +0100

    implement angular materials

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,40 +1,50 @@
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [PlayerComponent, MatButtonModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
 export class GameComponent {
   pickCardAnimation = false;
-  currentCard: string | undefined = ''; 
+  currentCard: string | undefined = '';
   game: Game;
 
   cards = [0, 1, 2, 3];
 
-  constructor() {
-    this.game = new Game()
+  constructor(public dialog: MatDialog) {
+    this.game = new Game();
   }
 
-  // newGame() {
-  //   this.game = new Game();
-  //   console.log(this.game);
-  // }
 
   takeCard() {
-    if (!this.pickCardAnimation){
-    this.currentCard = this.game.stack.pop();
-    this.pickCardAnimation = true;
-      
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+
       console.log(this.game.stack);
       console.log(this.game.playedCards);
-    setTimeout(() => {
-      this.game.playedCards.push(this.currentCard!);
-      this.pickCardAnimation = false;
-    }, 1000);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard!);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: { name: this.name, animal: this.animal },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
   }
 }
